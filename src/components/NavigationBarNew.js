6c354dc3d09d608855374329c3733fa93c12f544
Baxter Pearson <baxterrpearson@gmail.com>
2021-03-18 14:05:32 +0000

    More tiding, and icon added to nav. Plus deleted unused imgs

--- src/components/NavigationBarNew.js
+++ src/components/NavigationBarNew.js
@@ -6,6 +6,8 @@ import {
 import { Navbar, Nav, Container, Row, Col } from 'react-bootstrap'
 import styled from 'styled-components'
 
+import icon from '../assets/logo512.png';
+
 class NavigationBarNew extends React.Component{
 
     render(){
@@ -23,15 +25,14 @@ class NavigationBarNew extends React.Component{
                             <Col md={12}>
                             <Router>
                                     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
-                                        <Navbar.Brand href="/">Home</Navbar.Brand>
+                                        <Navbar.Brand href="/">
+                                            <img src={icon} height="35px" alt='where is it ??' />
+                                        </Navbar.Brand>
                                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                         <Navbar.Collapse id="basic-navbar-nav">
                                             <Nav className="mr-auto">
-                                            <Nav.Link href="/datagrid" style={{fontSize:"14pt"}}>Data Grid</Nav.Link>
-                                            <Nav.Link href="/barchart" style={{fontSize:"14pt"}}>Barchart</Nav.Link>
-
-                                                {/* <Nav.Item><NavLink exact activeClassName="active" to="/">Home</NavLink></Nav.Item>
-                                                <Nav.Item><NavLink activeClassName="active" to="/datagrid">Data Grid</NavLink></Nav.Item> */}
+                                                <Nav.Link href="/datagrid" style={{fontSize:"14pt"}}>Data Grid</Nav.Link>
+                                                <Nav.Link href="/barchart" style={{fontSize:"14pt"}}>Barchart</Nav.Link>
                                             </Nav>
                                         </Navbar.Collapse>
                                     </Navbar>
